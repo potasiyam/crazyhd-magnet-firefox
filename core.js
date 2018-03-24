88149e1926839bd93269cef71e35ecc736d41dd0
Tanbin Islam Siyam <potasiyam@gmail.com>
2018-03-24 20:06:38 +0600

    Personal crazyhd tracker support added

--- core.js
+++ core.js
@@ -7,17 +7,13 @@ function onError(error) {
 }
 
 function onGot(item) {
-    // console.log(item);
-    // var tracker = "";
-    // if (item.tracker) {
-    //   tracker = item.tracker;
-    // }
-    // else if (item[0].tracker) {
-    //   tracker = item[0].tracker;
-    // }
-
-    // console.log(tracker);
-
+    let trs = '';
+    if (typeof item.trackers === 'object') {
+        item.trackers.forEach(element => {
+            trs = trs + "&tr=" + encodeURI(element);
+        });
+    }
+    console.log(trs);
     var str = document.querySelector(".wrapper a.btn-download").getAttribute("href");
 
     while ((m = regex.exec(str)) !== null) {
@@ -26,28 +22,30 @@ function onGot(item) {
             regex.lastIndex++;
         }
 
-        if(document.querySelectorAll(".btn-chumbok").length > 0) {
+        if (document.querySelectorAll(".btn-chumbok").length > 0) {
             var buttons = document.querySelectorAll(".btn-chumbok");
             for (i = 0; i < buttons.length; ++i) {
                 buttons[i].remove();
             }
         }
-
         var aTag = document.createElement('A');
         aTag.className = ("btn btn-lg btn-danger btn-chumbok");
-        var mag_string = "magnet:?xt=urn:btih:" + m[1] + "&dn=" + encodeURI(m[2]);
-        aTag.setAttribute('href',mag_string);
+        var mag_string = "magnet:?xt=urn:btih:" + m[1] + "&dn=" + encodeURI(m[2]) + trs;
+        aTag.setAttribute('href', mag_string);
         aTag.innerHTML = "<i class=\"fa fa-magnet\"></i> Magnet";
 
         document.querySelector(".wrapper a.btn-download").parentNode.appendChild(aTag);
-        
+
     }
 
 }
 
-onGot({});
+var getting = browser.storage.local.get("trackers");
 
-console.log(typeof browser.storage);
-
-// var getting = browser.storage.local.get("tracker");
+getting.then(function(data) {
+    onGot(data);
+}, function(error) {
+    console.log('error', error);
+    onGot({});
+});
 // getting.then(onGot, onError);
